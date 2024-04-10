46bbb8b9f3886bd459277f01aa0b44b31df54918
Rajnish Shonkhia <rshonkhia65@gmail.com>
2024-04-10 16:26:35 +0530

    changes added

--- src/test/java/com/zee/ExpenseTracker/ExpenseTrackerApplicationTests.java
+++ src/test/java/com/zee/ExpenseTracker/ExpenseTrackerApplicationTests.java
@@ -1,9 +1,11 @@
-package com.zee.expenseTracker;
+package com.zee.ExpenseTracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration
 class ExpenseTrackerApplicationTests {
 
 	@Test
